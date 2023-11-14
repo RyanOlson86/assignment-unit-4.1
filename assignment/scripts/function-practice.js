379af1e03c0e20228a1ac3bb15f62f10684a240f
Ryan Olson <ryan.olson86@yahoo.com>
2023-11-14 16:19:21 -0600

    Added problem from codewars.com and solved

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -138,6 +138,26 @@ console.log('allPositive should return array [4, 8, 22]', allPositive([4, -10, 8
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
 
+// Timmy & Sarah think they are in love, but around where they live, 
+// they will only know once they pick a flower each. If one of the 
+// flowers has an even number of petals and the other has an odd number 
+// of petals it means they are in love. Write a function that will take 
+// the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log('Case: flower1 = 4 and flower2 = 3:', lovefunc(4,3));
+console.log('Case: flower1 = 4 and flower2 = 6:', lovefunc(4,6));
+console.log('Case: flower1 = 3 and flower2 = 3:', lovefunc(3,3));
+console.log('Case: flower1 = 3 and flower2 = 4:', lovefunc(3,4));
+
 // DO NOT MODIFY
 // Used for automated testing
 try {
